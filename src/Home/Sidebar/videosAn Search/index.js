f00e22a40ea75f10b99c4079bc446ec030deca50
Yaswanth <yaswanthram.396@gmail.com>
2024-09-17 10:26:15 +0530

    changes in NxtWatch

--- src/Home/Sidebar/videosAn Search/index.js
+++ src/Home/Sidebar/videosAn Search/index.js
@@ -56,7 +56,127 @@
 
 // export default GetApiRes;
 
+// import "./index.css";
+// import { ThreeDots } from "react-loader-spinner";
+// import Cookies from "js-cookie";
+// import React from "react";
+// import { FaSearch } from "react-icons/fa";
+
+// class GetApiRes extends React.Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       searchInput: "",
+//       DataApi: { videos: [] },
+//       loading: false, // New loading state
+//     };
+//   }
+
+//   componentDidMount = async () => {
+//     await this.fetchData(); // Call the fetchData method
+//   };
+
+//   fetchData = async () => {
+//     const cookieToken = Cookies.get("jwt_token");
+//     const { searchInput } = this.state;
+//     try {
+//       const response = await fetch(
+//         `https://apis.ccbp.in/videos/all?search=${searchInput}`, // Using search input dynamically
+//         {
+//           method: "GET",
+//           headers: {
+//             Authorization: `Bearer ${cookieToken}`,
+//           },
+//         }
+//       );
+
+//       if (!response.ok) {
+//         throw new Error("Failed to fetch data");
+//       }
+
+//       this.setState({ loading: true }); // Set loading true when fetching starts
+
+//       const data = await response.json();
+//       this.setState({ DataApi: data, loading: false }); //
+//       // const data = await response.json();
+//       // this.setState({ DataApi: data });
+//       // console.log(data);
+//     } catch (error) {
+//       console.error("Error fetching data:", error.message);
+//     }
+//   };
+
+//   handleSearchInput = (e) => {
+//     this.setState({ searchInput: e.target.value });
+//   };
+
+//   handleSubmit = (e) => {
+//     e.preventDefault();
+//     this.fetchData(); // Fetch data when submitting the search input
+//   };
+
+//   render() {
+//     const { searchInput, DataApi } = this.state;
+//     const videosArray = DataApi.videos;
+
+//     return (
+//       <>
+//         <div className="entireVideos">
+//           <form className="form" onSubmit={this.handleSubmit}>
+//             <input
+//               className="search"
+//               type="text"
+//               onChange={this.handleSearchInput}
+//               value={searchInput}
+//               placeholder="Search videos"
+//             />
+//             <div className="submitdiv" type="submit">
+//               <button className="submitOnButton">
+//                 <FaSearch type="submit" />
+//               </button>
+//             </div>
+//           </form>
+//           <div className="Videos">
+//             {videosArray.length > 0 ? (
+//               videosArray.map((item) => (
+//                 <div className="BG-container" key={item.id}>
+//                   <img
+//                     src={item.thumbnail_url}
+//                     alt="thumbnail_url"
+//                     className="thumbnail_url"
+//                   />
+//                   <div className="outer">
+//                     <img
+//                       src={item.channel.profile_image_url}
+//                       className="profile"
+//                       alt="profile_image_url"
+//                     />
+//                     <div className="inner">
+//                       <h1 className="heading">{item.title}</h1>
+//                       <p className="paragraphInThumb">{item.channel.name}</p>
+//                       <div className="count paragraphInThumb">
+//                         <p>{`${item.view_count} Views`}</p>
+//                         <p>{item.published_at}</p>
+//                       </div>
+//                     </div>
+//                   </div>
+//                 </div>
+//               ))
+//             ) : (
+//               <p>No videos found</p>
+//             )}
+//           </div>
+//         </div>
+//       </>
+//     );
+//   }
+// }
+
+// export default GetApiRes;
+
 import "./index.css";
+import { ThreeDots } from "react-loader-spinner";
 import Cookies from "js-cookie";
 import React from "react";
 import { FaSearch } from "react-icons/fa";
@@ -67,20 +187,24 @@ class GetApiRes extends React.Component {
 
     this.state = {
       searchInput: "",
-      DataApi: { videos: [] }, // Initialize as an object with a videos array
+      DataApi: { videos: [] },
+      loading: false, // Loading state added
     };
   }
 
   componentDidMount = async () => {
-    await this.fetchData(); // Call the fetchData method
+    await this.fetchData();
   };
 
   fetchData = async () => {
     const cookieToken = Cookies.get("jwt_token");
     const { searchInput } = this.state;
+
+    this.setState({ loading: true }); // Start loading
+
     try {
       const response = await fetch(
-        `https://apis.ccbp.in/videos/all?search=${searchInput}`, // Using search input dynamically
+        `https://apis.ccbp.in/videos/all?search=${searchInput}`,
         {
           method: "GET",
           headers: {
@@ -94,10 +218,10 @@ class GetApiRes extends React.Component {
       }
 
       const data = await response.json();
-      this.setState({ DataApi: data });
-      console.log(data);
+      this.setState({ DataApi: data, loading: false }); // Stop loading, set data
     } catch (error) {
       console.error("Error fetching data:", error.message);
+      this.setState({ loading: false }); // Stop loading in case of error
     }
   };
 
@@ -107,12 +231,13 @@ class GetApiRes extends React.Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
-    this.fetchData(); // Fetch data when submitting the search input
+    this.fetchData(); // Fetch data on search
   };
 
   render() {
-    const { searchInput, DataApi } = this.state;
+    const { searchInput, DataApi, loading } = this.state;
     const videosArray = DataApi.videos;
+    console.log(videosArray);
 
     return (
       <>
@@ -125,18 +250,28 @@ class GetApiRes extends React.Component {
               value={searchInput}
               placeholder="Search videos"
             />
-            {/* <button type="submit">Search</button> */}
             <div className="submitdiv" type="submit">
               <button className="submitOnButton">
-                {" "}
-                <FaSearch type="submit" />{" "}
+                <FaSearch type="submit" />
               </button>
             </div>
           </form>
+
           <div className="Videos">
-            {videosArray && videosArray.length > 0 ? (
+            {loading ? (
+              <div className="loader-container" data-testid="loader">
+                <ThreeDots
+                  height="80"
+                  width="80"
+                  radius="9"
+                  color="blue"
+                  ariaLabel="three-dots-loading"
+                  visible={true}
+                />
+              </div>
+            ) : videosArray.length > 0 ? (
               videosArray.map((item) => (
-                <div className="BG-container">
+                <div className="BG-container" key={item.id}>
                   <img
                     src={item.thumbnail_url}
                     alt="thumbnail_url"
@@ -149,9 +284,7 @@ class GetApiRes extends React.Component {
                       alt="profile_image_url"
                     />
                     <div className="inner">
-                      <h1 className="heading" key={item.id}>
-                        {item.title}
-                      </h1>
+                      <h1 className="heading">{item.title}</h1>
                       <p className="paragraphInThumb">{item.channel.name}</p>
                       <div className="count paragraphInThumb">
                         <p>{`${item.view_count} Views`}</p>
